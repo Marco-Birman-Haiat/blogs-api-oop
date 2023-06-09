65b01784d70e35cd5eb4b68236dab9fb4ae9eaf6
Marco Haiat <marco.haiat@gmail.com>
2023-06-09 10:41:46 -0300

    feat: jwt base functions and config

---
+++ src/utils/authFunctions.ts
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+
+const secret = process.env.JTW_SECRET || 'minhaSenhaMBH';
+
+type TokenPayload = {
+  id: number; 
+  email: string;
+}
+
+export interface JwtAuthorization {
+  verifyToken(token: string): string | jwt.JwtPayload;
+  createToken(payload: TokenPayload): string;
+}
+
+export default class JwtAuthorizationImpl implements JwtAuthorization {
+  verifyToken(token: string): string | jwt.JwtPayload {
+    return jwt.verify(token, secret)
+  }
+
+  createToken(payload: TokenPayload): string {
+    return jwt.sign({ data: payload }, secret)
+  }
+}
