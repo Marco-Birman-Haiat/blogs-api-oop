8c810c281da5dd67c84ee203b5ffe9612b88f387
Marco Haiat <marco.haiat@gmail.com>
2023-06-12 09:59:06 -0300

    feat: blog post controller update implementantion - and create correction

--- src/controllers/blogPost.controller.ts
+++ src/controllers/blogPost.controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from 'express';
 import { BlogPostService } from '../services/blogPost.service';
 import { JwtAuthorization } from '../utils/authFunctions';
+import getErrorCode from '../utils/httpError';
 
 export interface BlogPostController {
   create(req: Request, res: Response): Promise<Response>
   getAll(req: Request, res: Response): Promise<Response>
+  update(req: Request, res: Response): Promise<Response>
 }
 
 export default class BlogPostControllerImpl implements BlogPostController {
@@ -15,6 +17,9 @@ export default class BlogPostControllerImpl implements BlogPostController {
     const { id, email } = req.payload;
 
     const createdBlogPost = await this.blogPostService.create({ title, content, userId: id }, categoryIds)
+    if (createdBlogPost.type === 'UNPROCESSABLE_DATA') {
+      return res.status(getErrorCode(createdBlogPost.type)).json(createdBlogPost.data);
+    }
 
     return res.status(201).json(createdBlogPost);
   }
@@ -22,5 +27,17 @@ export default class BlogPostControllerImpl implements BlogPostController {
   async getAll(req: Request, res: Response): Promise<Response> {
     const allBlogPosts = await this.blogPostService.getAll();
     return res.status(200).json(allBlogPosts.data);
+  }
+
+  async update(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const { content, title } = req.body;
+
+    const updatedBlogPost = await this.blogPostService.update(id, { title, content });
+    if (updatedBlogPost.type === 'UNPROCESSABLE_DATA') {
+      return res.status(getErrorCode(updatedBlogPost.type)).json(updatedBlogPost.data);
+    }
+
+    return res.status(200).json(updatedBlogPost.data);
   }
 }
